b796f62e33b97f7cd670d9ac736515df452b8fe1
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-22 14:39:46 -0500

    Initial version (setup and data preparation)

---
+++ main.py
@@ -0,0 +1,16 @@
+# Setup
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+print(tf.__version__)
+
+# Prepare data
+num_classes = 100
+input_shape = (32, 32, 3)
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+
+print(f"x_train shape: {x_train.shape} - y_train shape: {y_train.shape}")
+print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
